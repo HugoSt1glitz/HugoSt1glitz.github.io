bc3d5a39d42a29239d22cb2230ddea97accae056
HugoSt1glitz <slow.lary@gmail.com>
2024-01-03 20:35:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-const element = document.querySelector('#example');
-  element.select();
-  element.setSelectionRange(0, 99999);
-  document.execCommand('copy');
-//window.close();
+navigator.permissions.query({name: "clipboard-write"})
+.then(({state}) => {
+    console.log(`permission response: ${state}`);
+    if (state === "granted") {
+        const data = [new ClipboardItem({ "text/plain": new Blob([`The time is ${new Date()}`], { type: "text/plain" }) })];
+        navigator.clipboard.write(data).then(
+            () => {
+                console.log("Clipboard write succeeded");
+            },
+            () => {
+                console.error("Clipboard write failed");
+            }
+        );
+    }
+});
