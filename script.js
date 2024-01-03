1696d67247e86c01ebd913552fc00f7fd0a61380
HugoSt1glitz <slow.lary@gmail.com>
2024-01-03 20:40:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ navigator.permissions.query({name: "clipboard-write"})
 .then(({state}) => {
     console.log(`permission response: ${state}`);
     if (state === "granted") {
-        const data = [new ClipboardItem({ "text/plain": new Blob([`The time is ${new Date()}`], { type: "text/plain" }) })];
+        const data = 'test';
         navigator.clipboard.write(data).then(
             () => {
                 console.log("Clipboard write succeeded");
